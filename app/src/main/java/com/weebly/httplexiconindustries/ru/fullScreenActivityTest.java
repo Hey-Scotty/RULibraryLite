04f2adf35c65ba3d5b5c484aa43714c432233db3
Hey-Scotty <tinytevin23@icloud.com>
2016-11-11 00:50:48 -0500

    adding fullscreen test
    committing

---
+++ app/src/main/java/com/weebly/httplexiconindustries/ru/fullScreenActivityTest.java
@@ -0,0 +1,13 @@
+package com.weebly.httplexiconindustries.ru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class fullScreenActivityTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen_test);
+    }
+}
