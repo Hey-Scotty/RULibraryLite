e5aed5a342b5e673800ee0ab09aa45f587803323
Hey-Scotty <tinytevin23@icloud.com>
2016-11-12 19:06:25 -0500

    added search results view

--- app/src/main/java/ru/SearchHomeActivity.java
+++ app/src/main/java/ru/SearchHomeActivity.java
@@ -1,14 +1,30 @@
 package ru;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class SearchHomeActivity extends AppCompatActivity {
-
+    Button searchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_home);
+
+        searchButton = (Button) findViewById(R.id.bSearch);
+
+        // Capture button clicks
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(SearchHomeActivity.this,
+                        ResultsActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
