9c68042be8ac35c50448356c8b14c4ac4db47d83
Lexicon370 <lexicon370@gmail.com>
2016-11-29 11:47:05 -0500

    Commit to sync
    Git hub is failing to sync the project.

--- app/src/main/java/ru/LoginActivity.java
+++ app/src/main/java/ru/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+;
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
         final Button bLogin = (Button) findViewById(R.id.bLogin);
