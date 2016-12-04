0907b20341e652677b165162ab1f4446cc115954
Hey-Scotty <Scotty@Tevins-MacBook-Pro.local>
2016-12-04 02:11:19 -0500

    Major update
    Search refinement working. Search results now represented with image
    and title text. Book Selected Activity Created. Commented majority of
    existing code. removed unnecessary classes, methods and lines of code.

--- app/src/main/java/com/weebly/httplexiconindustries/ru/ContentParsing/BookData.java
+++ app/src/main/java/com/weebly/httplexiconindustries/ru/ContentParsing/BookData.java
@@ -12,28 +12,29 @@ import java.io.IOException;
  */
 
 public class BookData {
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //CLASS INSTANCE VARIABLES
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     private String title;
     private String author;
     private String description;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //END OF CLASS INSTANCE VARIABLES
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     public BookData(String URL) throws IOException {
         Document doc = Jsoup.connect(URL).get();
-        title = getTitle(doc);
-        author = getAuthor(doc);
-        description = getDesc(doc);
-    }
-    public String getTitle() {
-        return title;
+        title = pullTitleData(doc);
+        author = pullAuthorData(doc);
+        description = pullAuthorData(doc);
     }
 
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-    
-    private String getAuthor(Document doc){
+    /**
+     * Gets the author's name for this specific book once a book has been selected to be viewed
+     * by the user
+     * @param doc html document that has the authors name
+     * @return the author or authors of this specific book
+     */
+    private String pullAuthorData(Document doc){
         String outputAuthor = "";
         Elements author = doc.getElementsByClass("by");
 
@@ -49,7 +50,13 @@ public class BookData {
         }
         return outputAuthor;
     }
-    private String getTitle(Document doc){
+    /**
+     * Gets the title of the for this specific book once a book has been selected to be viewed
+     * by the user
+     * @param doc doc html document that has the book name
+     * @return the title of this specific book
+     */
+    private String pullTitleData(Document doc){
         String outputTitle = "";
         Elements title = doc.getElementsByClass("title");
 
@@ -57,7 +64,13 @@ public class BookData {
         outputTitle = removeTags(holder.toString());
         return outputTitle;
     }
-    private String getDesc(Document doc) {
+    /**
+     * Gets the description of the for this specific book once a book has been selected to be viewed
+     * by the user
+     * @param doc: html document that has the description of the book
+     * @return the description of this specific book
+     */
+    private String pullDescriptionData(Document doc) {
         String outputDesc = "";
         Elements descData = doc.getElementsByClass("abstract");
         if (descData.size() > 0) {
@@ -75,7 +88,11 @@ public class BookData {
         return outputDesc;
     }
 
-
+    /**
+     * removes tag braces **<></>** from the strings. used in the pullTitleData and pullAuthorData methods
+     * @param withTags: the string before tags have been removed
+     * @return removed: the string after tags have been removed
+     */
     private String removeTags(String withTags){
 
 
@@ -93,6 +110,28 @@ public class BookData {
             }
         }
         return removed;
+    }
+
+    /**
+     * get this title class Attribute
+     * @return title: this title Attribute
+     */
+    public String getTitle() {
+        return title;
+    }
+    /**
+     * get this author class Attribute
+     * @return author: this author Attribute
+     */
+    public String getAuthor() {
+        return author;
+    }
+    /**
+     * get this description class Attribute
+     * @return description: this description Attribute
+     */
+    public String getDescription() {
+        return description;
     }
 
 
